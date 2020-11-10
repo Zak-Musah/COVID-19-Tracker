2734d7897b7597858a0449dcfec4c8355392fd39
Abdul-Razak <arazak4r@gmail.com>
2020-11-11 00:27:23 +0100

    Installed react-leaflet and added a map component

--- src/App.js
+++ src/App.js
@@ -1,47 +1,54 @@
-import React from 'react';
+import React from "react";
+import MapView from "./components/Map/MapView";
+import { Container, Row, Col } from "react-bootstrap";
 // import ReactDOM from 'react-dom';
 // import App from './App';
 
-import { Cards, Charts, CountryPicker } from './components';
-import styles from './App.module.css';
-import { fetchData } from './api';
+import { Cards, Charts, CountryPicker } from "./components";
+import styles from "./App.module.css";
+import { fetchData } from "./api";
 
-import coronImage from './images/images.jpg';
+import coronImage from "./images/images.jpg";
 
 class App extends React.Component {
-    //a state to push data to components
+  //a state to push data to components
 
-    state = {
-        data: {},
-        country: '',
-    }
+  state = {
+    data: {},
+    country: "",
+  };
 
-    async componentDidMount() {
-        const fetchedData = await fetchData();
+  async componentDidMount() {
+    const fetchedData = await fetchData();
 
-        this.setState({ data: fetchedData });
-    }
+    this.setState({ data: fetchedData });
+  }
 
-    handleCountryChange = async (country) => {
-        const fetchedData = await fetchData(country);
+  handleCountryChange = async (country) => {
+    const fetchedData = await fetchData(country);
 
-        this.setState({ data: fetchedData, country: country });
+    this.setState({ data: fetchedData, country: country });
+  };
 
-    }
-
-    render() {
-        //deconstruct before passing to the cards componentt
-        const { data, country } = this.state;
-        return (
-
-            <div className={styles.container}>
-                <img className={styles.image} src={coronImage} alt="COVID-19" />
-                <Cards data={data} />
-                <CountryPicker handleCountryChange={this.handleCountryChange} />
-                <Charts data={data} country={country} />
-            </div>
-        );
-    }
+  render() {
+    //deconstruct before passing to the cards componentt
+    const { data, country } = this.state;
+    return (
+      <Container fluid className="map-charts-container">
+        <Row>
+          <Col sm={12} md={6}>
+            <MapView />
+          </Col>
+          <Col sm={12} md={6}>
+            <img className={styles.image} src={coronImage} alt="COVID-19" />
+            <Cards data={data} />
+            <CountryPicker handleCountryChange={this.handleCountryChange} />
+            <Charts data={data} country={country} />
+          </Col>
+        </Row>
+      </Container>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Map/MapView.jsx
@@ -0,0 +1,20 @@
+import React, { useRef } from "react";
+import { MapContainer, TileLayer } from "react-leaflet";
+import styles from "./Map.module.css";
+
+function MapView() {
+  const map = useRef();
+
+  return (
+    <div className={styles.map}>
+      <MapContainer ref={map} center={[51.406502, 12.422398]} zoom={15}>
+        <TileLayer
+          attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+      </MapContainer>
+    </div>
+  );
+}
+
+export default MapView;
